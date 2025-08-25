6cc5c9a7b67e4d684146e19c2d1a0b9044a52e6b
Fergal Hennessy <fergalhennessy@berkeley.edu>
2025-08-25 16:18:01 -0700

    fixed options select

---
+++ src/select_options.ts
@@ -0,0 +1,35 @@
+import countyJSONRaw from './data/overlay.json'
+import type { Feature, FeatureCollection, Geometry} from 'geojson';
+
+type CountyProps = {
+  CountyDisplayName?: string;
+  HasLocationRestrictions?: number;
+  LocationRestrictions?: string;
+  HasAcreageRestrictions?: number;
+  AcreageRestrictions?: string;
+  HasGroundCoverRestrictions?: number;
+  GroundCoverRestrictions?: string;
+  HasScreeningRestrictions?: number;
+  ScreeningRestrictions?: string;
+  HasHeightRestrictions?: number;
+  HeightRestrictions?: string;
+  Website: string;
+};
+
+export function list_options(){
+
+    const countyFeatureCollection = countyJSONRaw as FeatureCollection<Geometry, CountyProps>
+
+    let counties = countyFeatureCollection.features
+        .map(f => {
+            return f.properties?.CountyDisplayName?? "";
+        })
+
+    counties.sort();
+    
+    return counties
+        .map(c => {
+            return `<option value="${c}">${c}</option>`
+        })
+        .join("\n");
+}
