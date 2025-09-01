48e4f18edb00874a7008c9699539e1298cf6876c
Fergal Hennessy <fergalhennessy@berkeley.edu>
2025-08-31 20:23:50 -0700

    add back button

--- src/county.ts
+++ src/county.ts
@@ -8,26 +8,23 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
 
 <br><br><br>
-
+  <a href="/" id="back-button"><img src="/back.png"></a>
   <div id="content">
     
   
     <h1>${el?.getAttribute("data-countyDisplayName")} Solar Zoning Regulations </h1>
 
     <br> 
-    <p> County Website: <a href="${el?.getAttribute("data-countyWebsite")}">${el?.getAttribute("data-countyWebsite")} </a> </p>
-    <p> ${el?.getAttribute("data-countyRegulationsDoc") === "" ? "No Document Available" : "<a href=\"" + el?.getAttribute("data-countyRegulationsDoc") + "\">Link to Regulations Document</a>"}
+    <p> ${el?.getAttribute("data-countyRegulationsDoc") === "" ? "No Regulation Document Available" : "<a href=\"" + el?.getAttribute("data-countyRegulationsDoc") + "\">Link to Regulations Document</a>"}
+    <p> <a href="${el?.getAttribute("data-countyWebsite")}">Main County Website</a> </p>
     <p> Location Restrictions: ${(el?.getAttribute("data-hasLocationRestriction") === null) ? "None" : el?.getAttribute("data-locationRestriction")} </p>
     <p> Acreage Restrictions: ${(el?.getAttribute("data-hasAcreageRestriction") === null) ? "None" : el?.getAttribute("data-acreageRestriction")} </p>
     <p> Ground Cover Restrictions: ${(el?.getAttribute("data-hasGroundCoverRestriction") === null) ? "None" : el?.getAttribute("data-groundCoverRestriction")} </p>
     <p> Screening Restrictions: ${(el?.getAttribute("data-hasScreeningRestriction") === null) ? "None" : el?.getAttribute("data-screeningRestriction")} </p>
     <p> Height Restrictions: ${(el?.getAttribute("data-hasHeightRestriction") === null) ? "None" : el?.getAttribute("data-heightRestriction")} </p>
     
-
-    <div id="map"></div>    
-  
+    <div id="map"></div>
   </div>
-  
 `
 
 const countyName = el?.getAttribute("data-countyDisplayName");
